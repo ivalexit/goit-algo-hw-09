adeede013e6fced05eb842f69cb5a9e97a6ac542
ivalexit <ivalextar@gmail.com>
2024-10-29 16:46:56 +0100

    def find_min_coins

--- hw9_1.py
+++ hw9_1.py
@@ -1,14 +1,42 @@
 import time
 
-def find_coins_greedy(amount, value):
+def find_coins_greedy(amount, values):
     coin_count = {}
-    value.sort(reverse=True)
+    values.sort(reverse=True)
 
-    for coin in value:
+    for coin in values:
         count = amount // coin
         if count > 0:
             coin_count[coin] = count
             amount -= count * coin
 
     return coin_count
+
+
+def find_min_coins(amount, values):
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    coin_used = [0] * (amount + 1)
+    
+    for coin in values:
+        for i in range(coin, amount + 1):
+            if dp[i - coin] + 1 < dp[i]:
+                dp[i] = dp[i - coin] + 1
+                coin_used[i] = coin
+
+    if dp[amount] == float('inf'):
+        return {} 
+
+    result = {}
+    while amount > 0:
+        coin = coin_used[amount]
+        if coin in result:
+            result[coin] += 1
+        else:
+            result[coin] = 1
+        amount -= coin
+
+    return result
+
+
 
