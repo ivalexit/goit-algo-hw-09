13c42d857da394647a68486cbc60dbbbff826f5a
ivalexit <ivalextar@gmail.com>
2024-10-29 15:58:06 +0100

    find_coins_greedy

---
+++ hw9_1.py
@@ -0,0 +1,14 @@
+import time
+
+def find_coins_greedy(amount, value):
+    coin_count = {}
+    value.sort(reverse=True)
+
+    for coin in value:
+        count = amount // coin
+        if count > 0:
+            coin_count[coin] = count
+            amount -= * coin
+
+    return coin_count
+
